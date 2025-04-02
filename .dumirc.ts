55aa6cb72c1be8c636949994e83f78ebb4b27caf
bootvue <bootvue@gmail.com>
2025-04-02 10:33:46 +0800

    doc config

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+  base: '/vbeats.github.io/',
+  publicPath: '/vbeats.github.io/',
   themeConfig: {
     name: 'vlog',
     footer: 'Powered by vbeats'
